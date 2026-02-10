fc53e52df282e2b49cf4e7a0f21316ff7d4f9af0
jakemay04 <jmay90@byu.edu>
2026-02-09 21:37:48 -0700

    create test for testing logout function

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -127,3 +127,34 @@ test('purchase with login', async ({ page }) => {
   // Check balance
   await expect(page.getByText('0.008')).toBeVisible();
 });
+
+test('purchase without login', async ({ page }) => {
+  await basicInit(page);
+    // Go to order page
+  await page.getByRole('button', { name: 'Order now' }).click();
+
+    // Create order
+  await expect(page.locator('h2')).toContainText('Awesome is a click away');
+  await page.getByRole('combobox').selectOption('4');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+  await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+  await page.getByRole('button', { name: 'Checkout' }).click();
+
+  await expect(page.getByText('Welcome back')).toBeVisible();
+  await expect(page.url()).toContain('/login');
+});
+
+test('comfirm logged out', async ({ page }) => {
+   //login
+  await basicInit(page);
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
+  //test logout
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page.getByRole('link', { name: 'Login' })).toBeVisible();
+});
