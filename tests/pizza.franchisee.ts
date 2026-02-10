d0b329c0ea3fecd719c21cbeac26fe41436d1b44
jakemay04 <jmay90@byu.edu>
2026-02-09 21:53:18 -0700

    mock out franchisee login functionallity

---
+++ tests/pizza.franchisee.ts
@@ -0,0 +1,36 @@
+import { test, expect } from 'playwright-test-coverage';
+import { Page } from '@playwright/test';
+import { Role, User } from '../src/service/pizzaService';
+
+async function basicInit(page: Page) {
+  let loggedInUser: User | undefined;
+  const validUsers: Record<string, User> = { 'f@jwt.com': { id: '3', name: 'Orem', email: 'f@jwt.com', password: 'f', roles: [{ role: Role.Franchisee }] } };
+  
+  await page.route('*/**/api/auth', async (route) => {
+    if (route.request().method() === 'DELETE') {
+      loggedInUser = undefined;
+      await route.fulfill({ status: 200 });
+      return;
+    }
+    const loginReq = route.request().postDataJSON();
+    const user = validUsers[loginReq.email];
+    if (!user || user.password !== loginReq.password) {
+      await route.fulfill({ status: 401, json: { error: 'Unauthorized' } });
+      return;
+    }
+    loggedInUser = validUsers[loginReq.email];
+    const loginRes = {
+      user: loggedInUser,
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    await route.fulfill({ json: loginRes });
+  });
+  await page.route('*/**/api/user/me', async (route) => {
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: loggedInUser });
+  });
+
+  
+
+});
